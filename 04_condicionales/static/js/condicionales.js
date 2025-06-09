c7e69fdb82412a8aa01311ccb133c46956e03a79
ami <ami.anab4@gmail.com>
2025-06-09 08:28:54 -0400

    okey

---
+++ 04_condicionales/static/js/condicionales.js
@@ -0,0 +1,22 @@
+function verificarEdad() {
+    let edad = 17;
+  
+    if (edad >= 18) {
+      alert("Es mayor de edad");
+    } else {
+      alert("Es menor de edad");
+    }
+  }
+  
+  function evaluarNota() {
+    let nota = 5;
+  
+    if (nota >= 7) {
+      alert("Excelente");
+    } else if (nota >= 4) {
+      alert("Aprobado");
+    } else {
+      alert("Reprobado");
+    }
+  }
+  
